f785bef85e7c31a7e570d20dbe06908edafea85e
VincenzoLaezza <43953126+VincenzoLaezza@users.noreply.github.com>
2018-12-10 11:40:58 +0000

    Update Studente.py

--- Studente.py
+++ Studente.py
@@ -1,21 +1,20 @@
-  classi = []
-  classe Studente :
-         def __init__self,nome,cognome,classe):
-         self.nome = nome
-         self.cognome = cognome
-         self.classe =classe
+classi = []
+classe Studente :
+       def __init__ (self,nome,cognome,classe):
+       self.nome = nome
+       self.cognome = cognome
+       self.classe =classe
  
  #main
  i=0
- num = input ("Quanti studenti ci sono?")
+ num = input ("Quanti studenti ci sono ?")
  num = int (num)
  while i<num:
-        nome = input ("inserisci il nome dello studente: ")
-        cognome = input ("inserisci il cognome :")
-        classe = input ("inserisci la classe: ")
-        Studenti = Studente(nome,cognome,classe)
-        classi.append (Sudenti)
-        classi [i] = Studenti
-        print (classi [i].nome, "//",classi[i].cognome,"//",classi[i].classe)
-        i+=1
-
+       nome = input ("inserisci il nome dello studente: ")
+       cognome = input ("inserisci il cognome :")
+       classe = input ("inserisci la classe: ")
+       Studenti = Studente(nome,cognome,classe)
+       classi.append (Sudenti)
+       classi [i] = Studenti
+       print (classi [i].nome, "//",classi[i].cognome,"//",classi[i].classe)
+       i+=1

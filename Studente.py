0ab9ae8b45c6edfbd47882ea1112ceb68c0f4b4f
VincenzoLaezza <vincenzogiaguaro@gmail.com>
2018-12-10 13:30:55 +0100

    esercizio

---
+++ Studente.py
@@ -0,0 +1,21 @@
+  classi = []
+  classe Studente :
+         def __init__self,nome,cognome,classe):
+         self.nome = nome
+         self.cognome = cognome
+         self.classe =classe
+ 
+ #main
+ i=0
+ num = input ("Quanti studenti ci sono?")
+ num = int (num)
+ while i<num:
+        nome = input ("inserisci il nome dello studente: ")
+        cognome = input ("inserisci il cognome :")
+        classe = input ("inserisci la classe: ")
+        Studenti = Studente(nome,cognome,classe)
+        classi.append (Sudenti)
+        classi [i] = Studenti
+        print (classi [i].nome, "//",classi[i].cognome,"//",classi[i].classe)
+        i+=1
+
